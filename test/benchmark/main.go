0b2ab65979504065e45f9db4a64de5f47890aa2c
fredele20 <victorisholaoladele@gmail.com>
2022-12-31 23:52:38 +0100

    working with tests

---
+++ test/benchmark/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+
+	"golang.org/x/exp/constraints"
+)
+
+type numeric interface {
+	constraints.Integer | constraints.Float
+}
+
+func sum[T numeric](numbers ...T) T {
+	var s T
+	for _, n := range numbers {
+		s += n
+	}
+	return s
+}
+
+func sumAny(numbers []interface{}) interface{} {
+	var result float64
+	for _, n := range numbers {
+		switch n.(type) {
+		case int:
+			result += float64(n.(int))
+		case float32, float64:
+			result += n.(float64)
+		default:
+			continue			
+		}
+	}
+
+	return result
+}
+
+func main() {
+	fmt.Println("hello")
+}
---
+++ test/benchmark/main_test.go
@@ -0,0 +1,17 @@
+package main
+
+import "testing"
+
+// write a benchmark for sum
+func BenchmarkSum(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		sum(1, 2, 3)
+	}
+}
+
+// write a benchmark for sumAny
+func BenchmarkSumAny(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		sumAny([]interface{}{1, 2, 3})
+	}
+}
