17d66803a9ffe7da97959c9f5ac8e7e2d8070aa4
Fredel <48722410+fredele20@users.noreply.github.com>
2023-01-03 12:53:44 +0000

    working with channels in concurrency

---
+++ concurrency/channels/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+)
+
+// sum - calculates and print the sum of numbers
+func sum(nums []int, ch chan<- int) {
+	sum := 0
+	for _, n := range nums {
+		sum += n
+	}
+	ch <- sum
+}
+
+func main() {
+
+	nums := []int{1, 2, 3, 4, 5}
+
+	ch := make(chan int)
+
+	go sum(nums, ch)
+	result := <-ch
+	fmt.Println("Result: ", result)
+
+	ch2 := make(chan string, 2)
+
+	ch2 <- "James"
+	ch2 <- "Victor"
+
+	fmt.Println(<-ch2)
+	fmt.Println(<-ch2)
+}
