a0a760a7f4de9fe466a3fd8a6d5ed63794aaeeca
Fredel <48722410+fredele20@users.noreply.github.com>
2023-01-03 13:12:59 +0000

    worked with channel-ranging in concurrency

---
+++ concurrency/channels-ranging/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+// fill a channel with ints up to max
+func fill(ch chan<- int, max int) {
+	// ensure non-deterministic random numbers
+	s := rand.NewSource(time.Now().UnixNano())
+	r := rand.New(s)
+
+	// loop and fill the channel up to its capacity with random numbers
+	for i := 0; i < cap(ch); i++ {
+		ch <- r.Intn(max)
+	}
+
+	// close the channel and signal that no more values will be sent
+	close(ch)
+}
+
+func main() {
+
+	// create a channel with a buffer size
+	numChan := make(chan int, 5)
+
+	// invoke the fill function as a goroutine
+	go fill(numChan, 100)
+
+	// range over the channel and print out the values
+	for num := range numChan {
+		fmt.Println(num)
+	}
+	
+}
